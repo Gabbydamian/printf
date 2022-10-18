a65d96dd6d3cdbf1c5d10dc27866061c03ef7d78
Daizy9 <itohowo203@gmail.com>
2022-10-18 11:01:26 -0700

    _print_un_int

---
+++ _print_octal.c
@@ -0,0 +1,15 @@
+#include "main"
+/**
+ * _print_octal - print octal
+ * @x:paramter or member 'x'
+ **/
+void _print_octal(int x)
+{
+	unsigned int c = x;
+
+	if (c / 8)
+	{
+		_print_octal(c / 8);
+	}
+	_putchar(c % 8 + '0');
+}
